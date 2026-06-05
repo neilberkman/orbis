87fc88b786c0b09eae27447b9ed3e66cc250cb1a
Neil Berkman <neil@xuku.com>
2026-06-05 14:01:06 -0700

    nif: add 0.9.1 precompiled checksums

---
+++ checksum-Elixir.Orbis.NIF.exs
@@ -0,0 +1,7 @@
+%{
+  "liborbis_nif-v0.9.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:fc491d6e77bf432b3807efc3180e88b206234b6c4db7c577c339ddb532dafe12",
+  "liborbis_nif-v0.9.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:268ab12a39a4055aa5390a37e87a5644c435f5569f192c8b59f0b0c75e4fcbde",
+  "liborbis_nif-v0.9.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:f61bee191a39ae2b7c86fb9bc91d3a8338eb0bfb46a09ea905719e10b7929441",
+  "liborbis_nif-v0.9.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:d510d22161293611c37722765e60a68b38b9f669f6600a1f0cbb744cf7a7f111",
+  "orbis_nif-v0.9.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:fd97095d12c55e8ed5d6a44d9ec787071ddabc60e8074d2aece34763261da0f7",
+}
