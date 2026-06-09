aba73482efa3624d93cc38d0de74c5b476ff6e83
Neil Berkman <neil@xuku.com>
2026-06-09 07:14:48 -0700

    Add precompiled NIF checksums for v0.14.1

--- checksum-Elixir.Orbis.NIF.exs
+++ checksum-Elixir.Orbis.NIF.exs
@@ -1,7 +1,7 @@
 %{
-  "liborbis_nif-v0.14.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:6125e13e61f30dea3c0f55a800cc471d14eef46fae6cffee7d3776975f741c02",
-  "liborbis_nif-v0.14.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:ae84687c4c7214e9364e2bcdf5d3d58ae06c97d82578b43c3346b6736eaa35c9",
-  "liborbis_nif-v0.14.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:bba36a5c4a4af7380803209d4442b202a5ef922f9b83abb938766c18f542bcae",
-  "liborbis_nif-v0.14.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:6b4401cdc37dc3820b56802ad30a1dd39d55358784fee381f9b006675e0a00eb",
-  "orbis_nif-v0.14.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:7f875e76d369386514d6a33a2f2674ff6e43eb4d23141a2cf76005e13c3621d1",
+  "liborbis_nif-v0.14.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:c4ab7e034e95569f81511b362562d586335af00beff42d5c60f2db114d109a61",
+  "liborbis_nif-v0.14.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:f25d056fcc3d9aa6fcb45ac0cfe777e0fc5dad3527e5254cb6c78345dcf84500",
+  "liborbis_nif-v0.14.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:183a8659719e7ed455e7b962bddff6b692735c1114176f2e2b820e84370dd92f",
+  "liborbis_nif-v0.14.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:3d4896f5a180a081b5695dbeba3ec7030fefaadb49cd0d2441015de23abb2dd9",
+  "orbis_nif-v0.14.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:cb7ce7c7d2280570f8e47749c6952daf604c020808145ef3ea36747ffa60d53e",
 }
